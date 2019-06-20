65611da18351745e8579010c2a0159afb5fa1821
Haomin Shi <haomins1@verifone.com>
2019-06-20 16:30:03 -0500

    init commit

---
+++ app/src/main/java/com/example/splitpanel/FragmentRight.kt
@@ -0,0 +1,15 @@
+package com.example.splitpanel
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentRight : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_right, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/splitpanel/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.splitpanel
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // val fragmentLeft = supportFragmentManager.findFragmentById(R.id.fragmentLeft)
+        // val fragmentRight= supportFragmentManager.findFragmentById(R.id.fragmentRight)
+    }
+}
